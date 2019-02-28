0a9929254cf8a2327409d366ab982464444d921c
Joey <joey.marien2000@gmail.com>
2019-02-28 14:35:24 +0100

    7.3 challenge

--- 7.3.js
+++ 7.3.js
@@ -1,1 +1,11 @@
-const readlineSync = require("readline-sync");let n = readlineSync.question("Division of: ");console.log(a(n));function a(n){let b=[],i;for(i=1;i<=n;i++){if(n%i==0){b.push(i);}}return b;}
+const readlineSync = require("readline-sync");let n = new Number(readlineSync.question("Division of: "));
+console.log(a(n));
+function a(n) {
+let b=[],i;
+for (i=1;i<=n;i++) {
+if (n%i==0) {
+b.push(i);
+}
+}
+return b;
+}
